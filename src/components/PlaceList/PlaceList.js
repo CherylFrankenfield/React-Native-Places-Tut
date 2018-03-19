6c2f2168e574c07a006d4864c31aa347af54619e
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-03-19 13:48:37 -0700

    Add scrollview to PlaceList comp

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {StyleSheet, ScrollView} from 'react-native';
 import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
@@ -12,9 +12,9 @@ const placeList = props => {
   ));
 
   return(
-    <View
+    <ScrollView
       style={styles.listContainer}>{placesOutput}
-    </View>
+    </ScrollView>
   );
 };
 
