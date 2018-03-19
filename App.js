3dfda5f53423fa57fb6c3f495f3d723fa9d17d33
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-03-19 13:28:17 -0700

    Fix = error in PlaceList comp and app successfully deletes a list item now

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet,View } from 'react-native';
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
 
@@ -16,11 +16,25 @@ export default class App extends React.Component {
     });
   };
 
+  placeDeletedHandler = index => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter((place, i) => {
+          return i !== index;
+        })
+      };
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList places={this.state.places}/>
+        <PlaceInput
+          onPlaceAdded={this.placeAddedHandler}
+        />
+        <PlaceList
+          places={this.state.places} onItemDeleted={this.placeDeletedHandler}
+        />
       </View>
     );
   }
