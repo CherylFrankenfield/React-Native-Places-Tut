c4fcd3a45b55f45db8ed1fa7b0743a4dd81a9b86
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-03-20 13:50:08 -0700

    Add static image to ListItem component and add styles

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet,View } from 'react-native';
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
+import placeImage from './src/assets/tagaytay.png';
 
 export default class App extends React.Component {
   state = {
@@ -13,7 +14,8 @@ export default class App extends React.Component {
       return {
         places: prevState.places.concat({
           key: Math.random(),
-          value: placeName
+          name: placeName,
+          image: placeImage
         })
       };
     });
