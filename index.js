e6a0135c483dc170f727f66132b2332b18a7c54b
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-04-01 16:00:01 -0700

    import Provider in App.js, import and configure store, and add store,Provider as arguments to every Screen that you want Redux

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import {Provider} from 'react-redux';
-import configureStore from './src/store/configureStore';
 import React from 'react';
+import configureStore from './src/store/configureStore';
 
 const store = configureStore();
 
