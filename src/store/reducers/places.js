c2ab09a2d09b79a13b9ed496e7f8cf71d5d57924
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-04-01 16:39:02 -0700

    Remove selectedPlace state slice from places.js reducer in initialState and DELECT_PLACE case b/c not necessary anymore

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,8 +1,7 @@
 import {ADD_PLACE, DELETE_PLACE} from '../actions/actionTypes';
 
 const initialState = {
-  places: [],
-  selectedPlace: null
+  places: []
 };
 
 const reducer = (state = initialState, action) => {
@@ -23,8 +22,7 @@ const reducer = (state = initialState, action) => {
           ...state,
           places: state.places.filter(place => {
             return place.key !== state.selectedPlace.key;
-          }),
-          selectedPlace: null
+          })
         };
       // case SELECT_PLACE:
       //   return {
