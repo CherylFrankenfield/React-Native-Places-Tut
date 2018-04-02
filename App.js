ec8111aa5beef5f6b6c64776c68c5191ba797a66
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-04-01 20:12:35 -0700

    Edit App.js and add Navigation.component for PlaceDetailScreen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {Navigation} from 'react-native-navigation';
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
+import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
 import {Provider} from 'react-redux';
 import configureStore from './src/store/configureStore';
 
@@ -12,6 +13,7 @@ const store = configureStore();
 Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen, store, Provider);
 Navigation.registerComponent('awesome-places.SharePlaceScreen', () => SharePlaceScreen, store, Provider);
 Navigation.registerComponent('awesome-places.FindPlaceScreen', () => FindPlaceScreen, store, Provider);
+Navigation.registerComponent('awesome-places.PlaceDetailScreen', () => PlaceDetailScreen);
 
 //Start a App
 Navigation.startSingleScreenApp({
--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {View,Image,Text,Button,StyleSheet,TouchableOpacity} from 'react-native';
+import {View,Image,Text,StyleSheet,TouchableOpacity} from 'react-native';
 // import Icon from 'react-native-vector-icons/Ionicons';
 
 const placeDetail = props => {
   return(
-    <View style={styles.modalContainer}>
+    <View style={styles.container}>
       <View>
         <Image source={props.selectedPlace.image} style={styles.placeImage}/>
         <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
@@ -15,14 +15,13 @@ const placeDetail = props => {
             <Icon size={30} name='ios-trash' color='red'/>
           </View>
         </TouchableOpacity>
-        <Button title='Close' onPress={props.onModalClosed}/>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  modalContainer: {
+  container: {
     margin: 22
   },
   placeImage: {
