797589ca7162e9e018af1c8a51cd86fb77245852
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-04-02 21:33:40 -0700

    Delete props from onPress TouchableOpacity in PlaceDetail and import Button and now app successfully deletes places from stack

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,23 +1,24 @@
 import React, {Component} from 'react';
-import {View,Image,Text,StyleSheet,TouchableOpacity} from 'react-native';
+import {View,Image,Text,StyleSheet,Button,TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {connect} from 'react-redux';
 import {deletePlace} from '../../store/actions/index';
 
 class PlaceDetail extends Component {
   placeDeletedHandler = () => {
-    this.onDeletePlace(this.props.selectedPlace.key)
+    this.props.onDeletePlace(this.props.selectedPlace.key);
+    this.props.navigator.pop();
   }
 
   render() {
     return(
       <View style={styles.container}>
         <View>
-          <Image source={props.selectedPlace.image} style={styles.placeImage}/>
-          <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
+          <Image source={this.props.selectedPlace.image} style={styles.placeImage}/>
+          <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
         </View>
         <View>
-          <TouchableOpacity onPress={props.onItemDeleted}>
+          <TouchableOpacity onPress={this.placeDeletedHandler}>
             <View style={styles.deleteButton}>
               <Icon size={30} name='ios-trash' color='red'/>
             </View>
