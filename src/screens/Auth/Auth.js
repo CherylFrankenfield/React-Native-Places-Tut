5341956a0f00fdcec9aa63afb089e01983cd11c8
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-04-08 10:27:21 -0700

    Style Auth file with input, inputContainer, stylesheet.create

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View,Text,Button,TextInput} from 'react-native';
+import {View,Text,Button,TextInput,StyleSheet} from 'react-native';
 import startMainTabs from '../MainTabs/startMainTabs';
 
 class AuthScreen extends Component {
@@ -9,16 +9,36 @@ class AuthScreen extends Component {
 
   render() {
     return(
-      <View>
-      <Text>Please Log In</Text>
+      <View style={styles.container}>
+        <Text>Please Log In</Text>
         <Button title='Swith to Log In' />
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input} underlineColorAndroid='transparent' placeholder='Your Email Address'/>
+          <TextInput style={styles.input} underlineColorAndroid='transparent' placeholder='User Password'/>
+          <TextInput style={styles.input} underlineColorAndroid='transparent' placeholder='Confirm Password'/>
+        </View>
         <Button title='Submit' onPress={this.loginHandler}/>
-        <TextInput placeholder='Your Email Address'/>
-        <TextInput placeholder='User Password'/>
-        <TextInput placeholder='Confirm Password'/>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: '#eee',
+    padding: 5,
+    margin: 8
+  }
+});
 
 export default AuthScreen;
