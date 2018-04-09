38b395265fe182028c670d3839d40a3746925529
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-04-08 20:45:22 -0700

    Refactor SharePlace screen, add PickImage comp and PickLocation comps, and import reusable components into SharePlace

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {View,TextInput,Button,StyleSheet} from 'react-native';
+import DefaultInput from '../UI/DefaultInput/DefaultInput';
 
 class PlaceInput extends Component {
   state = {
@@ -12,53 +13,41 @@ class PlaceInput extends Component {
     });
   };
 
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === '') {
-      return;
-    }
-    this.props.onPlaceAdded(this.state.placeName);
+//Do not need after refactoring w/o button in return.
+  // placeSubmitHandler = () => {
+  //   if (this.state.placeName.trim() === '') {
+  //     return;
+  //   }
+  //   this.props.onPlaceAdded(this.state.placeName);
+  // };
+
     //not set state after but more elegant solution - check validity of name inside PlaceInput component b/c makes more sense logically.
     // this.setState(prevState => {
     //   return {
     //     places: prevState.places.concat(prevState.placeName)
     //   };
     // });
-  };
-
 
   render() {
     return(
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.placeInput}
-          placeholder='Name an awesome place'
-          value={this.state.placeName}
-          onChangeText={this.placeNameChangedHandler}
-        />
-        <Button
-          style={styles.placeButton}
-          title='Add'
-          onPress={this.placeSubmitHandler}
-        />
-      </View>
+      <DefaultInput placeholder='Place Name'
+        value={this.state.placeName}
+        onChangeText={this.placeNameChangedHandler}
+      />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  inputContainer: {
-    // flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '100%'
-  },
-  placeInput: {
-    width: '70%'
-  },
-  placeButton: {
-    width: '30%'
-  }
-});
-
 export default PlaceInput;
+
+// <TextInput
+// style={styles.placeInput}
+// placeholder='Name an awesome place'
+// value={this.state.placeName}
+// onChangeText={this.placeNameChangedHandler}
+// />
+// <Button
+// style={styles.placeButton}
+// title='Add'
+// onPress={this.placeSubmitHandler}
+// />
